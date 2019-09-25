f9beb707e0b3aadb1ddb863abd742725821aaa01
Alfred Birketvedt <alfredbirketvedt@Alfreds-MacBook-Pro.local>
2019-09-25 18:02:34 +0200

    Initial commit, add header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <header className="header-text">Birketech.com</header>
+        </div>
+    )
+}
+
+export default Header
