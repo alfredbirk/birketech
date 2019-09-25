56c7d56b2702d4a72fbeabacf535c0dd0be05ab9
Alfred Birketvedt <alfredbirk@gmail.com>
2019-09-25 22:14:23 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header/Header'
+import Header from './Components/Header/Header';
 
 function App() {
   return (
