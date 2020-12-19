9f9be86ee66a4690ec2cdfbe3dfa8681714d42fa
Alfred Birketvedt <alfredbirk@gmail.com>
2020-12-19 14:01:46 +0100

    Make about sextion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Projects from './Components/Projects/Projects';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About />
       <Projects/>
     </div>
   );
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import avatar from '../Header/small.png';
+
+import "./About.css";
+
+
+ const About = () => {
+    return (
+        <div className="about">
+            <div className="about__container">
+                <img className="about__image" src={avatar} alt="Avatar" />
+                <div>
+                    <h2>Hi, I'm Alfred Birketvedt</h2>
+                    <p>I'm a passionate full-stack developer</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
