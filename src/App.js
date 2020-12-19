52456598c9d1795a785ee688002047c19ce8d876
Alfred Birketvedt <alfredbirk@gmail.com>
2020-12-19 14:27:41 +0100

    Remove import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
-import Projects from './Components/Projects/Projects';
+// import Projects from './Components/Projects/Projects';
 import About from './Components/About/About';
 
 function App() {
