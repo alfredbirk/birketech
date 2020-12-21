ffefb9ece9560901a9c935d721ae657d6cc058bd
Alfred Birketvedt <alfredbirk@gmail.com>
2020-12-21 22:09:57 +0100

    Add reveal animation

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,18 +3,21 @@ import avatar from '../Header/small.png';
 
 import "./About.css";
 
+import Fade from 'react-reveal/Fade'; 
 
  const About = () => {
     return (
-        <div className="about">
-            <div className="about__container">
-                <img className="about__image" src={avatar} alt="Avatar" />
-                <div>
-                    <h2>Hi, I'm Alfred Birketvedt</h2>
-                    <p>I'm a passionate full-stack developer currently working at Netlight.</p>
+        <Fade bottom duration={900}>
+            <div className="about">
+                <div className="about__container">
+                    <img className="about__image" src={avatar} alt="Avatar" />
+                    <div>
+                        <h2>Hi, I'm Alfred Birketvedt</h2>
+                        <p>I'm a passionate full-stack developer currently working at Netlight.</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Fade>
     )
 }
 
--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -16,104 +16,108 @@ import TeliaLogo from '../../Assets/Telia.png';
 import TelenorLogo from '../../Assets/Telenor.png';
 import FinnLogo from '../../Assets/Finn.png';
 
+import Zoom from 'react-reveal/Zoom'; 
+
 
 const Timeline2 = () => {
     return (
-        <div className="timeline">
-            {/* <h1>Professional experience</h1> */}
-            <div>
-            <Timeline>
-                {/* <TimelineItem>
+        <Zoom delay={50} duration={1100}>
+          <div className="timeline">
+              {/* <h1>Professional experience</h1> */}
+              <div>
+              <Timeline>
+                  {/* <TimelineItem>
+                    <TimelineOppositeContent>
+                      <Typography variant="body2" color="textSecondary">
+                        jan. 2020 - present
+                      </Typography>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator className="timeline__logo--kolonial-container">
+                      <TimelineDot color="primary" variant="default">
+                        <img className="timeline__logo timeline__logo--kolonial" src={KolonialLogo} alt="Avatar" />
+  
+                      </TimelineDot>
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="h6" component="h1">
+                          Kolonial
+                        </Typography>
+                    </TimelineContent>
+                  </TimelineItem> */}
+                  <TimelineItem>
+                    <TimelineOppositeContent>
+                      <Typography variant="body2" color="textSecondary">
+                        aug. 2019 - des. 2020
+                      </Typography>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator className="timeline__logo--telia-container">
+                      <TimelineDot color="primary">
+                        <img className="timeline__logo timeline__logo--telia" src={TeliaLogo} alt="Avatar" />
+                      </TimelineDot>
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="h6" component="h1">
+                          Telia
+                        </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                  <TimelineItem>
                   <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">
-                      jan. 2020 - present
-                    </Typography>
-                  </TimelineOppositeContent>
-                  <TimelineSeparator className="timeline__logo--kolonial-container">
-                    <TimelineDot color="primary" variant="default">
-                      <img className="timeline__logo timeline__logo--kolonial" src={KolonialLogo} alt="Avatar" />
-
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent>
-                      <Typography variant="h6" component="h1">
-                        Kolonial
+                      <Typography variant="body2" color="textSecondary">
+                        jun. 2019 - aug. 2019
                       </Typography>
-                  </TimelineContent>
-                </TimelineItem> */}
-                <TimelineItem>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator className="timeline__logo--telenor-container">
+                      <TimelineDot color="primary">
+                      <img className="timeline__logo timeline__logo--telenor" src={TelenorLogo} alt="Avatar" />
+                      </TimelineDot>
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="h6" component="h1">
+                          Telenor
+                        </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                  <TimelineItem>
                   <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">
-                      aug. 2019 - des. 2020
-                    </Typography>
-                  </TimelineOppositeContent>
-                  <TimelineSeparator className="timeline__logo--telia-container">
-                    <TimelineDot color="primary">
-                      <img className="timeline__logo timeline__logo--telia" src={TeliaLogo} alt="Avatar" />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent>
-                      <Typography variant="h6" component="h1">
-                        Telia
+                      <Typography variant="body2" color="textSecondary">
+                        jun. 2018 - aug. 2018
                       </Typography>
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">
-                      jun. 2019 - aug. 2019
-                    </Typography>
-                  </TimelineOppositeContent>
-                  <TimelineSeparator className="timeline__logo--telenor-container">
-                    <TimelineDot color="primary">
-                    <img className="timeline__logo timeline__logo--telenor" src={TelenorLogo} alt="Avatar" />
-                    </TimelineDot>
-                  </TimelineSeparator>
-                  <TimelineContent>
-                      <Typography variant="h6" component="h1">
-                        Telenor
+                    </TimelineOppositeContent>
+                    <TimelineSeparator className="timeline__logo--telenor-container">
+                      <TimelineDot color="primary">
+                      <img className="timeline__logo timeline__logo--telenor" src={TelenorLogo} alt="Avatar" />
+                      </TimelineDot>
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="h6" component="h1">
+                          Telenor
+                        </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                  <TimelineItem>
+                  <TimelineOppositeContent>
+                      <Typography variant="body2" color="textSecondary">
+                        jun. 2017 - aug. 2017
                       </Typography>
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">
-                      jun. 2018 - aug. 2018
-                    </Typography>
-                  </TimelineOppositeContent>
-                  <TimelineSeparator className="timeline__logo--telenor-container">
-                    <TimelineDot color="primary">
-                    <img className="timeline__logo timeline__logo--telenor" src={TelenorLogo} alt="Avatar" />
-                    </TimelineDot>
-                  </TimelineSeparator>
-                  <TimelineContent>
-                      <Typography variant="h6" component="h1">
-                        Telenor
-                      </Typography>
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">
-                      jun. 2017 - aug. 2017
-                    </Typography>
-                  </TimelineOppositeContent>
-                  <TimelineSeparator className="timeline__logo--finn-container">
-                    <TimelineDot color="primary">
-                      <img className="timeline__logo timeline__logo--finn" src={FinnLogo} alt="Avatar" />
-                    </TimelineDot>
-                  </TimelineSeparator>
-                  <TimelineContent>
-                      <Typography variant="h6" component="h1">
-                        Finn.no
-                      </Typography>
-                  </TimelineContent>
-                </TimelineItem>
-              </Timeline>
-            </div>
-        </div>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator className="timeline__logo--finn-container">
+                      <TimelineDot color="primary">
+                        <img className="timeline__logo timeline__logo--finn" src={FinnLogo} alt="Avatar" />
+                      </TimelineDot>
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="h6" component="h1">
+                          Finn.no
+                        </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                </Timeline>
+              </div>
+          </div>
+        </Zoom>
     )
 }
 
