410863a1c6b3881037277b77612f8a7b39684246
Alfred Birketvedt <alfredbirk@gmail.com>
2021-01-16 19:27:00 +0100

    Add snake

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import Header from './Components/Header/Header';
 // import Projects from './Components/Projects/Projects';
 import About from './Components/About/About';
 import Timeline2 from './Components/Timeline/Timeline';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/">
-
-      <div className="App">
-        <Header/>
-        <About />
-        <Timeline2 />
-        {/* <Projects/> */}
-      </div>
-    </Route >
-    <Route exact path="/snake" render={() => {window.location.href="https://eager-ritchie-5eba2d.netlify.app/"}} />
-    </Router>
+    <div className="App">
+      <Header/>
+      <About />
+      <Timeline2 />
+      {/* <Projects/> */}
+      <a href="https://eager-ritchie-5eba2d.netlify.app/">Snake</a>
+    </div>
   );
 }
 
