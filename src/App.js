89c1be74cdb1ad81e7e4d8c0ccb2e2ed2ef29f14
alfredbirk <alfredbirk@gmail.com>
2022-12-27 16:42:02 +0100

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 // import Projects from './Components/Projects/Projects';
-import About from './Components/About/About';
-import Timeline2 from './Components/Timeline/Timeline';
+// import About from './Components/About/About';
+// import Timeline2 from './Components/Timeline/Timeline';
 
 function App() {
   return (
